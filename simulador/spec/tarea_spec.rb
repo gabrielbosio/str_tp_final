2210f09d24e9fd14d16fbf69bd6df8b47b44c2f0
Guido Rombola <guidorombola@gmail.com>
2019-10-26 10:42:49 -0300

    Agrega atributo nombre a Tarea

--- simulador/spec/tarea_spec.rb
+++ simulador/spec/tarea_spec.rb
@@ -3,11 +3,16 @@ require_relative '../model/tarea'
 
 describe Tarea do
   describe 'modelo' do
-    tiempo_de_computo = 10
-    tarea = described_class.new(tiempo_de_computo)
+    let(:tiempo_de_computo) { 10 }
+    let(:nombre) { 'Tarea1' }
+    let(:tarea) { described_class.new(nombre, tiempo_de_computo) }
 
     it 'al inicializar una tarea, debe almacenar su tiempo de computo' do
       expect(tarea.tiempo_de_computo).to eq tiempo_de_computo
+    end
+
+    it 'al inicializar una tarea, debe almacenar su nombre' do
+      expect(tarea.nombre).to eq nombre
     end
   end
 end
