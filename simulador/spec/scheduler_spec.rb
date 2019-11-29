49207cf0f867ddae9581bafc05fc6b0c3765f7a0
Guido Rombolá <guidorombola@gmail.com>
2019-11-29 12:46:51 -0300

    Verifica planificacion con tres tareas

--- simulador/spec/scheduler_spec.rb
+++ simulador/spec/scheduler_spec.rb
@@ -10,6 +10,7 @@ describe Scheduler do
   let(:tarea_1) { Tarea.new('t1', 3, 5, 2) }
   let(:tarea_1_comenzando_en_cero) { Tarea.new('t1', 3, 5) }
   let(:tarea_2) { Tarea.new('t2', 2, 8) }
+  let(:tarea_3) { Tarea.new('t3', 1, 4) }
 
   describe 'planificar' do
     it 'debe planificar una sola instancia de la tarea1 si ingresa una sola tarea' do
@@ -39,6 +40,15 @@ describe Scheduler do
       scheduler = described_class.new([tarea_1, tarea_2])
       instancias_planificadas = scheduler.planificar.instancias_planificadas
       instancias_planificadas_esperadas = %w[t2 t1 t1 t2 t1 t2 t1]
+      (0..6).each do |i|
+        expect(instancias_planificadas[i].nombre).to eq(instancias_planificadas_esperadas[i])
+      end
+    end
+
+    it 'planificacion con tareas t1(3,5,2), t2(2,8) y t3(1,4) primeras 7 instancias' do
+      scheduler = described_class.new([tarea_1, tarea_2, tarea_3])
+      instancias_planificadas = scheduler.planificar.instancias_planificadas
+      instancias_planificadas_esperadas = %w[t2 t1 t3 t1 t2 t1 t3]
       (0..6).each do |i|
         expect(instancias_planificadas[i].nombre).to eq(instancias_planificadas_esperadas[i])
       end
